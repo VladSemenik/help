1fa2044307d9b12e370d1cdf4e078ee1d1e2cdd1
Vlad Semenik <vlad.semenik@gmail.com>
2018-03-16 00:06:19 +0300

    ''

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function count(s, pairs) {
   // your implementation
+    var n=1;
+    var result = 0;
+    s = s.split('');
+    for(var i=0; i<pairs.length; i++){
+        n *= Math.pow(pairs[i][0],pairs[i][1]);
+    }
+    function nod(m,n){
+        if(n>0){
+           var k = m%n;
+            return nod(n,k);
+           }
+        else{
+            return Math.abs(m);
+        }
+    }
+    return n/3;
 }
